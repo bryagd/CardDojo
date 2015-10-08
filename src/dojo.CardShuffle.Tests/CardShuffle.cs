89b2b58ea0f0defc815b7316aeb1a485f1ed1c4a
Gregory Bryant <bryagd@gmail.com>
2015-10-08 11:39:55 -0400

    initial commit

---
+++ src/dojo.CardShuffle.Tests/CardShuffle.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace dojo.CardShuffle.Tests
+{
+    [TestClass]
+    public class CardShuffle
+    {
+        [TestMethod]
+        public void DeckBuilder_Is_Correct_Length()
+        {
+            var deck = new Deck();
+            var expectedLenth = 52;
+
+            Assert.AreEqual(expectedLenth, deck.DeckBuilder().Length);
+        }
+
+        [TestMethod]
+        public void IsDeckValid_Should_Return_False()
+        {
+            var deck = new Deck();
+            var cards = new Card[4];
+            cards[0] = new Card("Harts", "1");
+            cards[1] = new Card("Harts", "1");
+            cards[2] = new Card("Harts", "2");
+            cards[3] = new Card("Harts", "9");
+
+            Assert.IsFalse(deck.IsDeckValid(cards));
+        }
+
+        [TestMethod]
+        public void Deck_Should_Be_Without_Duplicates_After_Shuffle()
+        {
+            var deck = new Deck();
+            var shuffledDeck = deck.Shuffle(deck.DeckBuilder());
+
+            Assert.IsTrue(deck.IsDeckValid(shuffledDeck));
+        }
+
+        [TestMethod]
+        public void Shffle_Ordered_Deck()
+        {
+            var deck = new Deck();
+            var orderedDeck = deck.DeckBuilder();
+            var shuffledDeck = deck.Shuffle(deck.DeckBuilder());
+
+            Assert.AreEqual(true, (orderedDeck[0] != shuffledDeck[0] ||
+                                   orderedDeck[1] != shuffledDeck[1] ||
+                                   orderedDeck[2] != shuffledDeck[2] ||
+                                   orderedDeck[3] != shuffledDeck[3] ||
+                                   orderedDeck[4] != shuffledDeck[4]));
+        }
+
+        [TestMethod]
+        public void Shuffle_Two_Deck_To_Insure_Decks_Are_Random()
+        {
+            var deck = new Deck();
+            var deck1 = deck.DeckBuilder();
+            var deck2 = deck.DeckBuilder();
+
+            var shuffledDeck1 = deck.Shuffle(deck1);
+            var shuffledDeck2 = deck.Shuffle(deck2);
+
+            Assert.AreNotEqual(shuffledDeck1, shuffledDeck2);
+        }
+    }
+}
---
+++ src/dojo.CardShuffle/Card.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+
+namespace dojo.CardShuffle
+{
+    public class Card
+    {
+        public Card(string suit, string rank)
+        {
+            this.Suit = suit;
+            this.Rank = rank;
+        }
+
+        public string Suit { get; set; }
+        public string Rank { get; set; }
+
+    }
+
+    public class Deck
+    {
+        private readonly string[] _suits = GetSuits;
+        private readonly string[] _rank = GetRanks;
+
+    
+        private static readonly Random _random = new Random();
+
+        public Card[] DeckBuilder()
+        {
+            var deck = new Card[52];
+            for (var i = 0; i < deck.Length; i++)
+            {
+                deck[i] = new Card(_suits[i/13], _rank[i % 13]);
+            }
+            return deck;
+        }
+        public Card[] Shuffle(Card[] deck)
+        {
+            var rand = _random;
+            for (var i = deck.Length; i > 1; i--)
+            {
+                var v = rand.Next(i);
+                var tmp = deck[v];
+                deck[v] = deck[i - 1];
+                deck[i - 1] = tmp;
+            }
+
+            return deck;
+        }
+
+        public bool IsDeckValid(Card[] deck)
+        {
+            var d = deck
+                .GroupBy(x => new {x.Suit, x.Rank}).Count(g => g.Count() > 1);
+            return d < 1;
+        }
+
+        internal static string[] GetSuits => new [] { "Hart", "Club", "Spade", "Diamond"};
+
+        internal static string[] GetRanks => new [] { "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King" };
+    }
+}
+
